df40b36aa1bf61b3e2f87971a698063733b92acc
kojirou <kojirouhtc@gmail.com>
2020-09-18 16:58:54 +0800

    add some more tests

--- Tests/CombinedDateTests/CombinedDateTests.swift
+++ Tests/CombinedDateTests/CombinedDateTests.swift
@@ -2,9 +2,9 @@ import XCTest
 @testable import CombinedDate
 
 final class CombinedDateTests: XCTestCase {
-  let date = CombinedDate(year: 2020, month: 12, day: 25)
 
   func testReleaseDate() {
+    let date = CombinedDate(year: 2020, month: 12, day: 25)
     XCTAssertEqual(date, date)
     XCTAssertEqual(date, .init(rawValue: date.rawValue))
 
@@ -13,5 +13,19 @@ final class CombinedDateTests: XCTestCase {
     measure {
       _ = CombinedDate(rawValue: rawValue)
     }
+  }
+
+  func testIncompleteDate() {
+    let noDayDate = CombinedDate(year: 2020, month: 10)
+    XCTAssertEqual(noDayDate.day, 0)
+
+    let noMonthDayDate = CombinedDate(year: 2020)
+    XCTAssertEqual(noMonthDayDate.month, 0)
+    XCTAssertEqual(noMonthDayDate.day, 0)
+  }
+
+  func testCompareDate() {
+    XCTAssertLessThan(CombinedDate(year: 2020, month: 10, day: 1),
+                      CombinedDate(year: 2020, month: 10, day: 2))
   }
 }
